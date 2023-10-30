1e0672b2d76129a9aadf08f16db98c992a9b56d5
jishnupumex <139556055+jishnupumex@users.noreply.github.com>
2023-10-30 17:49:02 +0530

    Initial commit

---
+++ src/test/java/com/supply/logistics/LogisticsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supply.logistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
