2a9dc995aaee32eb3733bc0d00884e4a4ca6e507
jishnupumex <139556055+jishnupumex@users.noreply.github.com>
2023-11-21 18:00:06 +0530

    Invoice pdf download functionality
    Download functionality of Order invoice added

--- src/main/java/com/supply/logistics/service/LogisticsService.java
+++ src/main/java/com/supply/logistics/service/LogisticsService.java
@@ -11,6 +11,8 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Optional;
 
 @Slf4j
@@ -21,13 +23,15 @@ public class LogisticsService {
     private final OrderUpdateService orderUpdateService;
     private final KafkaTemplate<String, UserOrders> kafkaTemplate;
     private final ObjectMapper objectMapper;
+    private final InvoiceService invoiceService;
 
     @KafkaListener(topics = "${spring.kafka.topic.name.OrderFulfillment}", groupId = "${spring.kafka.consumer.group-id.OrderFulfillment}")
-    public void consumeOrderFulfillmentTopic(String userOrdersAsString) throws JsonProcessingException {
+    public void consumeOrderFulfillmentTopic(String userOrdersAsString) throws IOException {
         UserOrders userOrders = objectMapper.readValue(userOrdersAsString, UserOrders.class);
-
         userOrders.setOrderStatus(OrderStatus.DELIVERED);
         userOrderRepository.save(userOrders);
         log.info("Order Status -> {}", userOrders.getOrderStatus());
+        log.info("Order Status is {} for OrderID {} ", userOrders.getOrderStatus(),userOrders.getUserOrderId());
+
     }
 }
--- src/main/java/com/supply/logistics/service/OrderUpdateService.java
+++ src/main/java/com/supply/logistics/service/OrderUpdateService.java
@@ -1,13 +1,14 @@
 package com.supply.logistics.service;
 
+
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.exc.StreamReadException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.supply.logistics.entity.Inventory;
 import com.supply.logistics.entity.OrderRequest;
 import com.supply.logistics.entity.OrderStatus;
 import com.supply.logistics.entity.UserOrders;
+import com.supply.logistics.repo.InventoryRepo;
 import com.supply.logistics.repo.UserOrderRepository;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
@@ -15,9 +16,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -25,23 +23,55 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class OrderUpdateService {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(OrderUpdateService.class);
-    private final KafkaTemplate<String, String> kafkaTemplate;
     private final UserOrderRepository userOrderRepository;
     private final ObjectMapper objectMapper;
-//    @KafkaListener(topics = "${spring.kafka.topic.name.OrderFulfillmentBySupplier}", groupId = "OrderFulfillmentBySupplierGroup")
-//    public void consumeOrderFulfillmentBySupplierTopic(String  orderRequestAsString) throws JsonProcessingException {
-//        OrderRequest orderRequest = objectMapper.readValue(orderRequestAsString, OrderRequest.class);
-//log.info(String.valueOf(orderRequest.getOrderId()));
-//        Optional<UserOrders> optionalUserOrder = userOrderRepository.findById(orderRequest.getOrderId());
-//
-//        if (optionalUserOrder.isPresent()) {
-//            UserOrders existingUserOrder = optionalUserOrder.get();
-//            existingUserOrder.setOrderStatus(OrderStatus.SHIPPED);
-//            userOrderRepository.save(existingUserOrder);
-//            log.info("Order Status -> {}", existingUserOrder.getOrderStatus());
-//        } else {
-//            log.info("Product not available");
-//        }
-//    }
+    private final InventoryRepo inventoryRepo;
+
+    @KafkaListener(topics = "${spring.kafka.topic.name.OrderFulfillmentBySupplier}", groupId = "OrderFulfillmentBySupplierGroup")
+    public void consumeOrderFulfillmentBySupplierTopic(String orderRequestAsString) throws JsonProcessingException {
+        OrderRequest orderRequest = objectMapper.readValue(orderRequestAsString, OrderRequest.class);
+        log.info("Seller update received -> {}", orderRequest);
+        log.info("Prod ID -> {} , Required Prod Qty {} of OrderID - {}", orderRequest.getProdNumber(), orderRequest.getRequiredQty(), orderRequest.getOrderId());
+
+        // Check if orderId is not null before calling findById
+        if (orderRequest.getOrderId() != null) {
+            Optional<UserOrders> optionalUserOrder = userOrderRepository.findById(orderRequest.getOrderId());
+
+            if (optionalUserOrder.isPresent()) {
+                UserOrders existingUserOrder = optionalUserOrder.get();
+                existingUserOrder.setOrderStatus(OrderStatus.AVAILABLE);
+                userOrderRepository.save(existingUserOrder);
+                log.info("Order Status: {}", existingUserOrder.getOrderStatus());
+                Optional<Inventory> optionalInventory = inventoryRepo.findByProdId(Math.toIntExact(existingUserOrder.getProdId()));
+
+                if (optionalInventory.isPresent()) {
+                    Inventory existingInventory = optionalInventory.get();
+                    log.info("Inventory Status of prod id {} before delivery: {} ", existingInventory.getProdId(), existingInventory.getProdQty());
+
+                    // Update the prodQty based on the difference
+                    existingInventory.setProdQty(existingInventory.getProdQty() - existingUserOrder.getProdQty());
+
+                    // Save the updated Inventory entry
+                    inventoryRepo.save(existingInventory);
+
+                    log.info("Inventory Status of prod id {} after delivery prod qty: {} ", existingInventory.getProdId(), existingInventory.getProdQty());
+
+                    // Call the method to update orderStatus to "Delivered"
+                    updateOrderStatusToDelivered(existingUserOrder);
+                } else {
+                    log.error("Inventory not found for prodId: {}", existingUserOrder.getProdId());
+                }
+            } else {
+                log.info("Product not available");
+            }
+        } else {
+            log.error("Order ID is null.");
+        }
+    }
+
+    private void updateOrderStatusToDelivered(UserOrders userOrders) {
+        userOrders.setOrderStatus(OrderStatus.DELIVERED);
+        userOrderRepository.save(userOrders);
+        log.info("Order Status is {} for OrderID {} ", userOrders.getOrderStatus(),userOrders.getUserOrderId());
+    }
 }
